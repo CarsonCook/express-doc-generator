6830ba327e374ba90f64d1eda264712c48d0a57d
Carson Cook <cook.carson38@gmail.com>
2020-11-01 19:29:37 -0500

    Use globs for files to scan
    Signed-off-by: Carson Cook <cook.carson38@gmail.com>

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const glob = require('glob');
 
 const fsFileFormat = 'utf8';
 
@@ -25,57 +26,59 @@ const startTag = configJson.startTag || 'start';
 const descriptionTag = configJson.descriptionTag + ' ' || 'description '; // expect a space between 'description' and the text
 const endTag = configJson.endTag || 'end';
 const anyChar = '[\\s\\S]';
-const files = configJson.files || [
-    'C:\\Users\\cookc\\Desktop\\Code\\qplan\\back-end\\server\\routing\\coursesRouting.js',
-    'C:\\Users\\cookc\\Desktop\\Code\\qplan\\back-end\\server\\routing\\gradRequirementsRouting.js',
+const paths = configJson.files || [
+    'C:/Users/cookc/Desktop/Code/qplan/back-end/server/routing/*Routing.js',
 ];
 
 fs.writeFileSync(outputFile, `#${projectName}\n${projectDescription}\n`); // Write to file instead of append to overwrite past data
 
-files.forEach(file => {
-    const fileContent = fs.readFileSync(file, fsFileFormat);
+paths.forEach(path => {
+    const files = glob.sync(path);
 
-    const apiRegex = new RegExp(`${tag} ${startTag}(?<api>${anyChar}*?)${tag} ${endTag}`, 'ig');
-    const descriptionRegex = new RegExp(`${tag} ${descriptionTag}(?<description>${anyChar}*?)$`, 'im');
-    const methodRegex = new RegExp(`${expressObject}\.(?<method>${httpMethods.join('|')})`, 'i');
-    const requestFieldsRegexes = {};
-    requestFields.forEach(field => {
-        requestFieldsRegexes[field] = (new RegExp(`${requestObject}\.${field}\.(?<${field}>[\\w\\d_\\-$]*)`, 'ig'));
-    });
+    files.forEach(file => {
+        const fileContent = fs.readFileSync(file, fsFileFormat);
 
-    // Capture all APIs
-    const matches = fileContent.matchAll(apiRegex);
-    for (const match of matches) {
-        const {groups: {api}} = match;
-
-        // Capture API endpoint
-        const {groups: {method}} = api.match(methodRegex);
-        const route = 'route';
-        fs.appendFileSync(outputFile, `##${method.toUpperCase()} ${route}\n`);
-
-        // Capture API description
-        const descriptionMatch = api.match(descriptionRegex);
-        if (descriptionMatch) {
-            const {groups: {description}} = descriptionMatch;
-            fs.appendFileSync(outputFile, `${description}\n`);
-        }
-
-        // Capture API request fields
-        Object.keys(requestFieldsRegexes).forEach(key => {
-            const matches = [...api.matchAll(requestFieldsRegexes[key])];
-            if (matches.length > 0) {
-                fs.appendFileSync(outputFile, `###Request ${key}:\n`);
-                for (const match of matches) {
-                    fs.appendFileSync(outputFile, `* ${match.groups[key]}\n`);
-                }
-                fs.appendFileSync(outputFile, '\n');
-            }
+        const apiRegex = new RegExp(`${tag} ${startTag}(?<api>${anyChar}*?)${tag} ${endTag}`, 'ig');
+        const descriptionRegex = new RegExp(`${tag} ${descriptionTag}(?<description>${anyChar}*?)$`, 'im');
+        const methodRegex = new RegExp(`${expressObject}\.(?<method>${httpMethods.join('|')})`, 'i');
+        const requestFieldsRegexes = {};
+        requestFields.forEach(field => {
+            requestFieldsRegexes[field] = (new RegExp(`${requestObject}\.${field}\.(?<${field}>[\\w\\d_\\-$]*)`, 'ig'));
         });
-    }
+
+        // Capture all APIs
+        const matches = fileContent.matchAll(apiRegex);
+        for (const match of matches) {
+            const {groups: {api}} = match;
+
+            // Capture API endpoint
+            const {groups: {method}} = api.match(methodRegex);
+            const route = 'route';
+            fs.appendFileSync(outputFile, `##${method.toUpperCase()} ${route}\n`);
+
+            // Capture API description
+            const descriptionMatch = api.match(descriptionRegex);
+            if (descriptionMatch) {
+                const {groups: {description}} = descriptionMatch;
+                fs.appendFileSync(outputFile, `${description}\n`);
+            }
+
+            // Capture API request fields
+            Object.keys(requestFieldsRegexes).forEach(key => {
+                const matches = [...api.matchAll(requestFieldsRegexes[key])];
+                if (matches.length > 0) {
+                    fs.appendFileSync(outputFile, `###Request ${key}:\n`);
+                    for (const match of matches) {
+                        fs.appendFileSync(outputFile, `* ${match.groups[key]}\n`);
+                    }
+                    fs.appendFileSync(outputFile, '\n');
+                }
+            });
+        }
+    });
 });
 
 //TODO get the route
-//TODO handle directory locations and specific files
 //TODO get privileges?
 //TODO after @express-api-doc start allow setting express and request object names
 //TODO handle no match cases
